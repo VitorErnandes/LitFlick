4858d3616b6ed0cb0d6faf5142563ca48d7a9904
VitorErnandes <vitor.contas40@gmail.com>
2024-06-10 23:09:59 -0300

    add Icons in the bottom tabs route

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,35 @@
 import { Tabs } from 'expo-router';
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
 
 export default function TabsLayout() {
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
-        options={{ title: 'Início', headerShown: false }}
+        options={{
+          title: 'Início', headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name={'home-outline'} color={color} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="books"
-        options={{ title: 'Livros' }}
+        options={{
+          title: 'Livros',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name={'book-outline'} color={color} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="series"
-        options={{ title: 'Séries' }}
+        options={{ title: 'Séries',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name={'play-outline'} color={color} />
+          ),
+         }}
       />
     </Tabs>
   );
